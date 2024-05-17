06f8101ed8841a009f679fe311436f36ddee4ddb
Azhan Khan <1byteword@gmail.com>
2024-05-17 01:26:17 -0500

    bg color keyframe

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
 import React from 'react';
 import './App.css';
 import profileImage from './icons/profile.jpg';  // Update the path as needed
-import backgroundImage from './images/background.jpg'; 
 
 function App() {
   return (
     <div className="App">
-      <div 
-        className="background-image"
-        style={{
-          backgroundImage: `url(${backgroundImage})`,
-          backgroundSize: 'cover',
-          backgroundRepeat: 'no-repeat',
-          backgroundPosition: 'center',
-          position: 'fixed',
-          top: 0,
-          left: 0,
-          width: '100vw',
-          height: '100vh',
-          zIndex: -1,
-        }}
-      />
+      <div className="bg-pan-right" />
       <header className="App-header">
         <img src={profileImage} alt="Azhan Khan" className="profile-image"/>
         <h1 className="name">Azhan Khan</h1>
@@ -34,15 +19,14 @@ function App() {
         </section>
         <section className="App-education">
           <h2>Education</h2>
-              <div>
-                <h3>Georgia Institute of Technology</h3>
-                <p>Bachelor of Science in Computer Science</p>
-                <p>Took heavy coursework in Intelligence and Modeling/Simulation. Competed in ACM ICPC as a representative of Georgia Tech.</p>
-                <p>Computer Vision • Deep Learning • Systems and Networks • Computational Modeling Algorithms</p>
-              </div>
+          <div>
+            <h3>Georgia Institute of Technology</h3>
+            <p>Bachelor of Science in Computer Science</p>
+            <p>Took heavy coursework in Intelligence and Modeling/Simulation. Competed in ACM ICPC as a representative of Georgia Tech.</p>
+            <p>Computer Vision • Deep Learning • Systems and Networks • Computational Modeling Algorithms</p>
+          </div>
         </section>
         <section className="App-work">
-          
           <h2>Work Experience</h2>
           <div>
             <h3>DevOps Engineer - Financial Services Client</h3>
@@ -70,7 +54,6 @@ function App() {
             <p>pronounced "nightmare"</p>
             <p>Working with the National Security Innovation Network and the MIT AI Accelerator to create AI applications for the US Air Force's Joint All-Domain Operations.</p>
             <p>https://www.nsin.mil/  https://aia.mit.edu/</p>
-            
             <p>PyTorch • Endpoint Governance • TypeScript • Docker • CI/CD</p>
           </div>
         </section>
